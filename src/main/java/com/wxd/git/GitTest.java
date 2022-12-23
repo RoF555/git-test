0d9c23bf6ccdfa0a7d226d31ce73a10e22b8bb44
RoF <1605422857@qq.com>
2022-12-23 12:24:46 +0800

    first commit

---
+++ src/main/java/com/wxd/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.wxd.git;
+
+/**
+ * @Author:rof
+ * @Date:2022/12/23 - 12 - 23 - 12:21
+ * @Description:com.wxd.git
+ * @version: 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
